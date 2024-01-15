123239c9385fc15d3ab443ca4744aa26cb66c538
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-01-15 20:47:20 +0530

    import java.util.*; public class vinod {     public static void main(String[] args)     {         Scanner sc =new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         int x=(a*2)-b;         System.out.println(x);     } }

---
+++ Find_Second_Number.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int x=(a*2)-b;
+        System.out.println(x);
+    }
+}
