ccccd81f039da70d979f46a3ad554e46390e62de
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-01-15 20:51:35 +0530

    import java.util.*; public class vinod {     public static void main(String[] args)     {         Scanner sc =new Scanner(System.in);         String a=sc.nextLine();         int decimal=Integer.parseInt(a,8);         System.out.println(decimal);
        }
    }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        String a=sc.nextLine();
+        int decimal=Integer.parseInt(a,8);
+        System.out.println(decimal);
+        
+    }
+}
