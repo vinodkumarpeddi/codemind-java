5d06836fb622492a5facfab944d5de6dcf2bc315
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-01-15 21:01:38 +0530

    import java.util.*; public class vinod {     public static void main(String[] args)     {         Scanner sc =new Scanner(System.in);         int a=sc.nextInt();         double b=a*2.54;         System.out.printf("%.2f",b);     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        double b=a*2.54;
+        System.out.printf("%.2f",b);
+    }
+}
