4fa6f052fb1687f8cb08c157f4bf487c9a2688cc
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-01-15 20:29:26 +0530

    import java.util.Scanner; public class vinod {     public static void main(String[] args)     {         Scanner sc =new Scanner(System.in);         int a =sc.nextInt();         int b=sc.nextInt();         int c=b/a;         System.out.println(c);     } }

---
+++ Required_Points:.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class vinod
+{
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+        int a =sc.nextInt();
+        int b=sc.nextInt();
+        int c=b/a;
+        System.out.println(c);
+    }
+}
