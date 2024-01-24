69a9e319929ae098348ed3c3c3b71cd85baf6ddb
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-01-24 10:18:54 +0530

    import java.util.*; public class vinod {     public static void main(String[] args)     {         Scanner sc =new Scanner(System.in);          int x=sc.nextInt();           int b=sc.nextInt();            int a=sc.nextInt();         int c=(3*x)-a-b;         System.out.println(c);
        }
    }

---
+++ Average_weight.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String[] args)
+    {
+        Scanner sc =new Scanner(System.in);
+         int x=sc.nextInt();
+          int b=sc.nextInt();
+           int a=sc.nextInt();
+        int c=(3*x)-a-b;
+        System.out.println(c);
+        
+    }
+}
