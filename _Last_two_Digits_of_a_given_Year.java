edd32c28216e6db75a73c76bfbbf607979851cd3
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-01-24 10:32:01 +0530

    import java.util.*; public class vinod {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int yr=sc.nextInt();         int r=yr%100;         System.out.printf("%02d",r);     } }

---
+++ _Last_two_Digits_of_a_given_Year.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int yr=sc.nextInt();
+        int r=yr%100;
+        System.out.printf("%02d",r);
+    }
+}
