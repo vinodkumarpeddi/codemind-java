521e5ee0e90dcec158793af7a078bebc266b5633
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-01-31 11:44:46 +0530

    import java.util.*; public class vinod {     public static void main(String args[])     {         Scanner sc =new Scanner(System.in);         int b=sc.nextInt();         int s=sc.nextInt();          int t=sc.nextInt();          long  dc=2*b*s*t*512;         System.out.println(dc);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        int b=sc.nextInt();
+        int s=sc.nextInt();
+         int t=sc.nextInt();
+         long  dc=2*b*s*t*512;
+        System.out.println(dc);
+    }
+}
