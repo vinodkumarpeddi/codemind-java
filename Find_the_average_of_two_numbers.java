c4601b40c552119f1fed9549ef2827e5af8a029d
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-01-31 10:02:10 +0530

    import java.util.*; public class vinod {     public static void main(String args[])     {         Scanner sc =new Scanner(System.in);         int a=sc.nextInt();         int b=sc.nextInt();         double c=(a+b)/2.0;         System.out.printf("%.4f",c);     } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        double c=(a+b)/2.0;
+        System.out.printf("%.4f",c);
+    }
+}
