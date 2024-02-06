ac4c83f876b66ce627558e842850837a494860f1
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-02-06 11:56:53 +0530

    import java.util.*;
    public class Vinod {
        public static void main(String args[]) {
            Scanner sc = new Scanner(System.in);
            int n = sc.nextInt();
    
            if (n >= 18) {
                System.out.println("YES");
            } else {
                System.out.println("NO");
            }
        }
    }

---
+++ Casting_the_Vote.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Vinod {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n >= 18) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
