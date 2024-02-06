8ebaa13f90ec0348abe952e374eb6290e23ae642
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-02-06 23:50:49 +0530

    import java.util.*; public class vinod {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         int y=sc.nextInt();         int z=Math.abs(x-y);         System.out.println(z);     } }

---
+++ Donation.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        int z=Math.abs(x-y);
+        System.out.println(z);
+    }
+}
