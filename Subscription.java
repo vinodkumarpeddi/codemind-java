40b870eb72016b199aa865cefb514de3c8f1ed9b
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-02-06 23:47:43 +0530

    import java.util.*; public class vinod {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int x=sc.nextInt();         if(x >= 40)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Subscription.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        if(x >= 40)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
