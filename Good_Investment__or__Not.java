15b676493b954cdb123f993c69f0af8a70336755
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-02-06 22:44:47 +0530

    import java.util.*; public class vinod {     public static void main(String args[])     {         Scanner sc=new Scanner (System.in);         int a=sc.nextInt();         int b=sc.nextInt();         if(a >= 2*b)         {             System.out.println("YES");         }         else         {             System.out.println("NO");         }     } }

---
+++ Good_Investment__or__Not.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner (System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        if(a >= 2*b)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+            System.out.println("NO");
+        }
+    }
+}
