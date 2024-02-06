6054aca6aef1305a1f209948e67d2b4b3b1be288
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-02-06 23:26:11 +0530

    import java.util.*; public class vinod {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         double h=sc.nextDouble();         if(h<150)         {             System.out.println("Person is Dwarf.");         }         else if(h>150 && h<=165)         {              System.out.println("Person is average heighted.");         }         else if(h>165 && h<=195)         {              System.out.println("Person is taller.");         }         else         {              System.out.println("Abnormal height.");         }     } }

---
+++ Categorize.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        double h=sc.nextDouble();
+        if(h<150)
+        {
+            System.out.println("Person is Dwarf.");
+        }
+        else if(h>150 && h<=165)
+        {
+             System.out.println("Person is average heighted.");
+        }
+        else if(h>165 && h<=195)
+        {
+             System.out.println("Person is taller.");
+        }
+        else
+        {
+             System.out.println("Abnormal height.");
+        }
+    }
+}
