e3b73c147186181d92ee5eea407a73bbf934d42c
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-02-06 11:03:31 +0530

    import java.util.*; public class vinod {     public static void main(String args[])     {         Scanner sc =new Scanner(System.in);         int x=sc.nextInt();         int a=x*4;         if(a<=1000)         {             System.out.println("YES");         }         else         {              System.out.println("NO");         }     } }

---
+++ Tickets.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String args[])
+    {
+        Scanner sc =new Scanner(System.in);
+        int x=sc.nextInt();
+        int a=x*4;
+        if(a<=1000)
+        {
+            System.out.println("YES");
+        }
+        else
+        {
+             System.out.println("NO");
+        }
+    }
+}
