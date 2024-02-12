dcc667a71febbca82982b113a94147c4ddb43953
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-02-12 10:21:45 +0530

    import java.util.*;
    public class vinod {
        public static void main(String args[]) {
            Scanner sc = new Scanner(System.in);
           int x=sc.nextInt();
           if(x > 0)
           {
               System.out.println("Positive Number");
           }
           else
           {
               System.out.println("Negative Number");
           }
        }
    }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class vinod {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+       int x=sc.nextInt();
+       if(x > 0)
+       {
+           System.out.println("Positive Number");
+       }
+       else
+       {
+           System.out.println("Negative Number");
+       }
+    }
+}
