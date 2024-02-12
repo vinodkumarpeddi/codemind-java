487852ecbe3bd3c38bb59100439e20c5644c0820
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-02-12 10:17:36 +0530

    import java.util.*;
    public class vinod {
        public static void main(String args[]) {
            Scanner sc = new Scanner(System.in);
            char ch = sc.next().charAt(0);
            
            if (ch >= 'A' && ch <= 'Z') {
                System.out.println("uppercase alphabet");
            } else if (ch >= 'a' && ch <= 'z') {
                System.out.println("lowercase alphabet");
            } else {
                System.out.println("not an alphabet");
            }
        }
    }

---
+++ Character_is_Uppercase_or_Lowercase_.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class vinod {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        
+        if (ch >= 'A' && ch <= 'Z') {
+            System.out.println("uppercase alphabet");
+        } else if (ch >= 'a' && ch <= 'z') {
+            System.out.println("lowercase alphabet");
+        } else {
+            System.out.println("not an alphabet");
+        }
+    }
+}
