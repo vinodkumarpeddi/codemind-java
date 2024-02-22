2927218eec9aefb9643208852ef898040995b3e9
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-02-22 11:33:50 +0530

    import java.util.*; public class vinod {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s1=sc.nextLine();         String s2=sc.nextLine();         if(s1.equals(s2))         {             System.out.println("Strings are Equal");         }         else         {              System.out.println("Strings are not Equal");         }     } }

---
+++ Compare_Two_Strings.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1=sc.nextLine();
+        String s2=sc.nextLine();
+        if(s1.equals(s2))
+        {
+            System.out.println("Strings are Equal");
+        }
+        else
+        {
+             System.out.println("Strings are not Equal");
+        }
+    }
+}
