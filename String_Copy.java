859fcd43c5326e3e6c845e7ec339c058012852de
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-02-23 10:33:04 +0530

    import java.util.*;
    public class vinod {
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
            String s=sc.nextLine();
            String s1=new String(s);
            System.out.println(s1);
    
           
        }
    }

---
+++ String_Copy.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class vinod {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s=sc.nextLine();
+        String s1=new String(s);
+        System.out.println(s1);
+
+       
+    }
+}
