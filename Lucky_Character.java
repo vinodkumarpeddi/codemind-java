33a9d0f669197767ff2ba7d4a0ae7460077d5cc2
vinodkumarpeddi <119839782+vinodkumarpeddi@users.noreply.github.com>
2024-02-23 10:04:05 +0530

    import java.util.*; public class vinod {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s=sc.nextLine();         char lucky=s.charAt(6);         System.out.println(lucky);     } }

---
+++ Lucky_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class vinod
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        char lucky=s.charAt(6);
+        System.out.println(lucky);
+    }
+}
